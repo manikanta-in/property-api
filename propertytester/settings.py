2ab4fedd2b7e56afa1efca77bf5bcf32b37c9402
manikanta <kuchipudimanikantachowdary1@gmail.com>
2022-05-01 10:46:15 +0530

    docker changes

--- propertytester/settings.py
+++ propertytester/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
     # },
      'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'properties',
+        'NAME': 'property',
         'USER': 'root',
-        'PASSWORD': 'susmitha',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
